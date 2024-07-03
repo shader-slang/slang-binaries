21916551db1faee368eb555e4b5108b744a006ac
Ellie Hermaszewska <ellieh@nvidia.com>
2024-07-03 09:09:05 +0800

    use only cross compiles from build script

--- spirv-tools/build.sh
+++ spirv-tools/build.sh
@@ -1,23 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
+shopt -s nullglob
 
-x64=$(nix build --print-out-paths .#cross.musl64.out)
-i686=$(nix build --print-out-paths .#cross.musl32.out)
-aarch64=$(nix build --print-out-paths .#cross.aarch64-multiplatform-musl.out)
-w64=$(nix build --print-out-paths .#cross.mingwW64.out)
-w32=$(nix build --print-out-paths .#cross.mingw32.out)
+go() {
+  out=$(nix build --print-out-paths ".#cross.$1.out")
+  mkdir -p "./$2/bin"
+  cp "$out"/bin/{spirv-dis*,spirv-val*,*.dll} "./$2/bin"
+  chmod -R u+w "$2"
+}
 
-mkdir -p ./x86_64-linux/bin/
-mkdir -p ./i686-linux/bin/
-mkdir -p ./aarch64-linux/bin/
-mkdir -p ./windows-x64/bin/
-mkdir -p ./windows-x86/bin/
-
-cp "$x64"/bin/{spirv-dis,spirv-val} ./x86_64-linux/bin/
-cp "$i686"/bin/{spirv-dis,spirv-val} ./i686-linux/bin/
-cp "$aarch64"/bin/{spirv-dis,spirv-val} ./aarch64-linux/bin/
-cp "$w64"/bin/{*.dll,spirv-dis.exe,spirv-val.exe} ./windows-x64/bin/ && 
-  chmod -R u+w ./windows-x64
-cp "$w32"/bin/{*.dll,spirv-dis.exe,spirv-val.exe} ./windows-x86/bin/ &&
-  chmod -R u+w ./windows-x86
+go musl64 x86_64-linux
+go musl32 i686-linux
+go aarch64-multiplatform-musl aarch64-linux
+go mingw32 windows-x86
+go mingwW64 windows-x64
